ec94de860a2ff151e0746a39b155588aef3b44e1
FHoltorf <32248677+FHoltorf@users.noreply.github.com>
2023-04-24 01:20:20 -0400

    new experiments

--- synthetic_data.jl
+++ synthetic_data.jl
@@ -2,12 +2,12 @@ cd(@__DIR__)
 using Pkg
 Pkg.activate(".")
 
-using StochasticAD, Random, Distributions, Optimisers, CairoMakie, LinearAlgebra
+using StochasticAD, Random, Distributions, Optimisers, CairoMakie, LinearAlgebra, DynamicPPL
 
 Random.seed!(123)
 
 # number of sites
-n = 25
+n = 5
 
 # number of visits per site
 K = [4 for i in 1:n]
@@ -44,6 +44,17 @@ function conditional(y,z,αβ;W=W,X=X)
         end
     end
     return p 
+end
+function logconditional(y,z,αβ;W=W,X=X)
+    logp = 0.0
+    @views α, β = αβ[1:2], αβ[3:4]
+    for i in eachindex(X)
+        logp += z[i]*log(ψ(X[i],β)) + (1-z[i])*log(1-ψ(X[i], β))
+        for j in eachindex(W[i])
+            logp += y[i][j]*log(z[i]*d(W[i][j], α))+ (1-y[i][j])*log(1-z[i]*d(W[i][j], α))
+        end
+    end
+    return logp 
 end
 function joint(y,z,αβ,prior;W=W, X=X)
     return conditional(y,z,αβ;W=W,X=X)*prior(αβ)
@@ -99,6 +110,7 @@ function q(ϕ,z,αβ; z_known=z_known)
     ps = [z_known[i] == 1 ? 1 : 1/2*(1+0.99*tanh(ϕ[20+i])) for i in eachindex(X)]
     return pdf(q_αβ, αβ)*prod((ps .^ z) .* ((1 .- ps) .^ (1 .- z)))
 end
+
 function logq(ϕ,z,αβ; z_known=z_known)
     @views α, β = αβ[1:2], αβ[3:4]
     μ = ϕ[1:4]
@@ -107,7 +119,7 @@ function logq(ϕ,z,αβ; z_known=z_known)
     #q_αβ = MvNormalCanon(μ, Σinv*μ, Σinv)
     q_αβ = MvNormal(μ, V*V' + 0.01*I)
     ps = [z_known[i] == 1 ? 1 : 1/2*(1+0.95*tanh(ϕ[20+i])) for i in eachindex(X)]
-    return logpdf(q_αβ, αβ) + sum(@. log((ps ^ z) * ((1 - ps) ^ (1 .- z))))
+    return logpdf(q_αβ, αβ) + sum(@. log((ps ^ z) * ((1 - ps) ^ (1 - z))))
 end
 
 function sample_q(ϕ; X=X, W=W, z_known=z_known)
@@ -136,11 +148,19 @@ function elbo_estimator(ϕ; n=1, y=Y, prior=prior, W=W, X=X)
     end
     return elbo/n
 end
-
+function predictive_posterior_estimator(ϕ; n=1, y=Y, W=W, X=X)
+    log_predictive = 0.0
+    for _ in 1:n
+        z, αβ = sample_q(ϕ)
+        log_predictive += logconditional(y, z, αβ, W=W, X=X)
+    end
+    return log_predictive/n
+end
 function optimize_elbo(batch_size, iterations, LEARNING_RATE; n_estimator = batch_size, n_snapshots = 1)
     ϕ0 = randn(n+20)
     obj = StochasticModel(ϕ -> -elbo_estimator(ϕ, n= batch_size), ϕ0)
     elbo_trace = Float64[]
+    log_predictive_trace = Float64[]
     wall_times = Float64[]
     ϕ_trace = Vector{Float64}[]
     optimizer = Adam(LEARNING_RATE)
@@ -158,32 +178,19 @@ function optimize_elbo(batch_size, iterations, LEARNING_RATE; n_estimator = batc
         end
         if i % n_snapshots == 0
             elbo = elbo_estimator(obj.p, n=n_estimator)
+            log_predictive = predictive_posterior_estimator(obj.p, n=n_estimator)
+            push!(log_predictive_trace, log_predictive)
             push!(elbo_trace, elbo)
             push!(wall_times, t)
         end
     end
     elbo = elbo_estimator(obj.p, n=n_estimator)
+    log_predictive = predictive_posterior_estimator(obj.p, n=n_estimator)
+    push!(log_predictive_trace, log_predictive)
     push!(elbo_trace, elbo)
     push!(wall_times, t)
-    return obj.p, ϕ_trace, wall_times, elbo_trace
+    return obj.p, ϕ_trace, wall_times, elbo_trace, log_predictive_trace
 end
-
-function plot_opt_trajectories(batch_sizes, traces)
-    fig = Figure(fontsize=18)
-    ax = Axis(fig[1,1], xlabel = "Iterations", ylabel = "ELBO", xscale=log2)
-    for batch_size in batch_sizes
-        lines!(ax, traces[batch_size], label = "batch size = $batch_size", linewidth=2)
-    end
-    axislegend(position=:rb)
-    fig
-end
-
-# now an asymptotically correct Markov Chain approach
-include("turing_model.jl")
-
-n_chain = 100000
-sampler = NUTS(1000, 0.65)
-chain = sample(occupancy(αβ_prior,Y,z_known,W,X), sampler, n_chain, drop_warmup=true)
 
 function plot_chain(chain; burnin = 1000)
     fig = Figure(fontsize=28)
@@ -305,7 +312,7 @@ function plot_results_movie(chain, ϕ_trace, true_pars; filename = "VI_movie.mp4
     Colorbar(ga[2,1], cf, #ticks=(collect(-width:1:0), ["$i" for i in abs.(-width:1:0)]),
                       label = L"\propto \log \, q", width=30)
     Label(ga[2,1,Top()], "Variational Distribution", font = :bold, padding = (0, 0, 10, 0))
-    it_label = Observable(1)
+    #it_label = Observable(1)
     #gb = fig[3, 2] = GridLayout()
     #it = lift(i -> string("Iteration = ", it_label.val), 1)
     #Label(gb[1,1], it)#, font = :bold)
@@ -327,21 +334,57 @@ function plot_results_movie(chain, ϕ_trace, true_pars; filename = "VI_movie.mp4
     end
 end
 
+# sampling
+include("turing_model.jl")
+
+n_chain = 100000
+sampler = NUTS(50,0.65) # HMC(0.05, 1000) 
+chain = sample(occupancy(Y), sampler, n_chain, drop_warmup=false)
+#MCMC_log_predictive_trace = cumsum(chain[:log_density][:,1]) ./ (1:length(chain[:log_density]))
+MCMC_log_predictive_trace = cumsum([logmarginal(Y,z_known,ab,W=W,X=X) for ab in eachrow(chain[chain.name_map[1]].value.data[:,:,1])]) ./ (1:length(chain[:log_density]))
+MCMC_times = range(0.0, chain.info[2]-chain.info[1], length=length(MCMC_log_predictive_trace))
+
+# p(theta|Y) p(Y) = p(Y, theta) = p(Y|theta) p(theta)
+# P(Y) = p(Y|theta) * p(theta)/p(theta|Y) 
+
 # get a trajectory
 # compare VI trajectories
-# batch size = 1
-ϕ_opt, ϕ_trace, times, elbo_trace = optimize_elbo(1, 500, 0.05, n_snapshots=1000, n_estimator = 1000)
-fig = plot_results(chain, ϕ_opt, burnin=25000, vcat(α_true, β_true), thinning = 100)
-save("../figures/progress_report.pdf",fig)
-plot_results_movie(chain, ϕ_trace, burnin=25000, vcat(α_true, β_true), filename = "VI_movie_1.mp4", thinning = 100)
-
 # batch size = 10
-ϕ_opt, ϕ_trace, times, elbo_trace = optimize_elbo(10, 500, 0.05, n_snapshots=1000, n_estimator = 1000)
+ϕ_opt, ϕ_trace, times, elbo_trace, log_predictive_trace = optimize_elbo(10, 500, 0.05, n_snapshots=1000, n_estimator = 1000)
 fig = plot_results(chain, ϕ_opt, burnin=25000, vcat(α_true, β_true), thinning = 100)
 save("../figures/progress_report.pdf",fig)
 plot_results_movie(chain, ϕ_trace, burnin=25000, vcat(α_true, β_true), filename = "VI_movie_10.mp4", thinning = 100)
 
+# batch size = 1
+ϕ_opt, ϕ_trace, times, elbo_trace, log_predictive_trace = optimize_elbo(1, 500, 0.05, n_snapshots=10, n_estimator = 1000)
+fig = plot_results(chain, ϕ_opt, burnin=25000, vcat(α_true, β_true), thinning = 100)
+save("../figures/progress_report.pdf",fig)
+plot_results_movie(chain, ϕ_trace, burnin=25000, vcat(α_true, β_true), filename = "VI_movie_1.mp4", thinning = 100)
+
+fig = Figure(fontsize=28)
+ax = Axis(fig[1,1],xlabel=L"\text{time}",ylabel=L"\text{log predictive posterior}", xscale=log10)
+lines!(ax, MCMC_times[2:end], MCMC_log_predictive_trace[2:end])
+lines!(ax, times[2:end], log_predictive_trace)
+fig
+
+results = Dict()
+n_batches = [1,10,50]
+for n in n_batches
+    ϕ_opt, ϕ_trace, times, elbo_trace, log_pred_trace = optimize_elbo(n, 200, 0.05, n_snapshots=5, n_estimator = 1000)
+    results[n] = (ϕ_trace, times, elbo_trace, log_pred_trace)
+end
+
+
+# compare convergence in terms of log posterior predictive
+#n_estimator = [1, 10, 50]
+#for n in n_estimator
+#    ϕ_opt, ϕ_trace, times, elbo_trace = optimize_elbo(1, 500, 0.05, n_snapshots=1000, n_estimator = 1000)
+
+#end
+
+
 # plot convergence
+#=
 batch_sizes = [1, 10, 50]
 n_iterations = 100
 step_size = 0.05
@@ -356,6 +399,16 @@ for (i,batch_size) in enumerate(batch_sizes)
     end
     results[batch_size] = Dict(:mean => [mean(r) for r in eachcol(sample_traces)],
                                :std => [std(r) for r in eachcol(sample_traces)])
+end
+
+function plot_opt_trajectories(batch_sizes, traces)
+    fig = Figure(fontsize=18)
+    ax = Axis(fig[1,1], xlabel = "Iterations", ylabel = "ELBO", xscale=log2)
+    for batch_size in batch_sizes
+        lines!(ax, traces[batch_size], label = "batch size = $batch_size", linewidth=2)
+    end
+    axislegend(position=:rb)
+    fig
 end
 
 function plot_sgd_traces(results; colors = [:red, :blue, :purple, :dodgerblue, :orange],
@@ -378,4 +431,4 @@ function plot_sgd_traces(results; colors = [:red, :blue, :purple, :dodgerblue, :
 end
 fig = plot_sgd_traces(results, alpha = 0.2)
 save("../figures/sgd_traces.pdf",fig)
-s
+=#
--- turing_model.jl
+++ turing_model.jl
@@ -1,6 +1,6 @@
 using Turing
 
-@model function occupancy(prior,Y,z_known,W,X)
+@model function occupancy(Y;z_known = z_known, prior = αβ_prior, W=W, X=X)
     αβ ~ prior
     Turing.@addlogprob! logmarginal(Y,z_known,αβ,W=W,X=X)
 end    
