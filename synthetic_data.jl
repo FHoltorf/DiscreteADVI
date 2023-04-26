76b598cf895abf35b99c61a2cadf9d159108433c
FHoltorf <32248677+FHoltorf@users.noreply.github.com>
2023-04-26 14:39:05 -0400

    change to reduced model and add Gibbs sampler for comparison

--- synthetic_data.jl
+++ synthetic_data.jl
@@ -1,4 +1,4 @@
-include("model.jl") # model specifications 
+include("model_reduced.jl") # model specifications 
 include("utils.jl") # optimization routines
 include("visualizations.jl") # plotting routines
 
@@ -26,36 +26,25 @@ Z = [rand(Bernoulli(ψ(X[i], β_true))) for i in 1:n] # true latent variables
 Y = [[rand(Bernoulli(Z[i]*d(W[i][j], α_true))) for j in 1:K[i]] for i in 1:n] # observations
 z_known = [!all(Y[i] .== 0) for i in 1:n] # flag for which latent variables are known with certainty
 
-# evaluation
-n_chain = 1000
+# MCMC inference
+n_chain = 100000
 sampler = NUTS() # HMC(0.05, 1000) 
 t_NUTS = @elapsed chain_NUTS = sample(marginal_occupancy(Y), sampler, n_chain, drop_warmup=false)
 NUTS_log_predictive_trace = cumsum([logmarginal(Y,z_known,ab,W=W,X=X) for ab in eachrow(chain_NUTS[chain_NUTS.name_map[1]].value.data[:,:,1])]) ./ (1:length(chain_NUTS[:log_density]))
 NUTS_times = range(0.0, t_NUTS, length=length(NUTS_log_predictive_trace))
+plot_chain(chain_NUTS, burnin=25000)
 
-plot_chain(chain_NUTS, burnin=1)
-
-sampler = Gibbs(HMC(0.01,200,:αβ), PG(2, :z)) 
-t_Gibbs = @elapsed chain_Gibbs = sample(occupancy(Y), sampler, 10000)
-Gibbs_log_predictive_trace = cumsum([logmarginal(Y,z_known,ab,W=W,X=X) for ab in eachrow(chain_Gibbs[chain_Gibbs.name_map[1]].value.data[:,:,1])]) ./ (1:length(chain_Gibbs[:log_density]))
-plot_chain(chain_Gibbs, burnin=2000)
-
-
-# sampler = PG(20) 
-# x = ones(10)#randn(10)
-# t_PG = @elapsed chain_PG = sample(gdemo(x), sampler, 100)
-# sampler = NUTS()
-# t_PG = @elapsed chain_NUTS = sample(gdemo(x), sampler, 100)
-
-# fig = Figure()
-# ax = Axis(fig[1,1])
-# hist!(ax, chain_PG[:m].data[:])
-# hist!(ax, chain_NUTS[:m].data[:])
-# fig
+sampler = Gibbs(HMC(0.05,1000,:αβ), PG(2, :z)) 
+t_Gibbs = @elapsed chain_Gibbs = sample(occupancy(Y), sampler, 20000, init_theta = vcat(zeros(4), ones(n-sum(z_known))))
+#Gibbs_loglikelihood = [isnan(loglikelihood(Y,pars[5:end],pars[1:4],W=W,X=X)) ? -1000 : loglikelihood(Y,pars[5:end],pars[1:4],W=W,X=X) for pars in eachrow(chain_Gibbs[chain_Gibbs.name_map[1]].value.data[:,:,1])]
+Gibbs_loglikelihood = [loglikelihood(Y,pars[5:end],pars[1:4],W=W,X=X) for pars in eachrow(chain_Gibbs[chain_Gibbs.name_map[1]].value.data[:,:,1])]
+sum(isnan.(Gibbs_loglikelihood))
+Gibbs_log_predictive_trace = cumsum(Gibbs_loglikelihood) ./ (1:length(Gibbs_loglikelihood))
+Gibbs_times = range(0.0, t_Gibbs, length=length(Gibbs_log_predictive_trace))
+plot_chain(chain_Gibbs, burnin=5000)
 
 # compare VI trajectories
-ϕ0 = randn(20+n)
-
+ϕ0 = randn(20+n-sum(z_known))
 struct OptResults
     ϕ_opt
     ϕ_trace
@@ -65,15 +54,15 @@ struct OptResults
 end
 # batch size = 10
 res10 = OptResults(optimize_elbo(deepcopy(ϕ0), 10, 500, 0.05, n_snapshots=10, n_estimator = 1000)...)
-fig = pairplot(chain, res10.ϕ_opt, burnin=25000, vcat(α_true, β_true), thinning = 100)
+fig = pairplot(chain_NUTS, res10.ϕ_opt, burnin=25000, vcat(α_true, β_true), thinning = 100)
 save("../figures/VI_synthetic_10.pdf",fig)
-pairplot_movie(chain, res10.ϕ_trace, burnin=25000, vcat(α_true, β_true), filename = "VI_movie_10.mp4", thinning = 100)
+pairplot_movie(chain_NUTS, res10.ϕ_trace, burnin=25000, vcat(α_true, β_true), filename = "VI_movie_10.mp4", thinning = 100)
 
 # batch size = 1
 res1 = OptResults(optimize_elbo(deepcopy(ϕ0), 1, 500, 0.05, n_snapshots=10, n_estimator = 1000)...)
-fig = pairplot(chain, res1.ϕ_opt, burnin=25000, vcat(α_true, β_true), thinning = 100)
+fig = pairplot(chain_NUTS, res1.ϕ_opt, burnin=25000, vcat(α_true, β_true), thinning = 100)
 save("../figures/VI_synthetic_1.pdf",fig)
-pairplot_movie(chain, res1.ϕ_trace, burnin=25000, vcat(α_true, β_true), filename = "VI_movie_1.mp4", thinning = 100)
+pairplot_movie(chain_NUTS, res1.ϕ_trace, burnin=25000, vcat(α_true, β_true), filename = "VI_movie_1.mp4", thinning = 100)
 
 # compare posterior predictive
 fig = Figure(fontsize=28)
@@ -82,7 +71,11 @@ ax = Axis(fig[1,1], xlabel = "time [s]",
                     xminorticks = IntervalsBetween(8),
                     ylabel = "log predictive posterior",
                     xscale = log10)
-lines!(ax, NUTS_times[2:end], NUTS_log_predictive_trace[2:end], color = :black, linewidth = 2)
-lines!(ax, res1.times[2:end], res1.log_predictive_trace, color = :red, linewidth=2)
-lines!(ax, res10.times[2:end], res10.log_predictive_trace, color = :blue, linewidth=2)
+ylims!(ax, -80, -30)
+lines!(ax, NUTS_times[2:end], NUTS_log_predictive_trace[2:end], color = :black, linewidth = 2, label = "NUTS + marginalization")
+lines!(ax, Gibbs_times[2:end], Gibbs_log_predictive_trace[2:end], color = :orange, linewidth = 2, label = "Gibbs(HMC, PG)")
+lines!(ax, res1.times[2:end], res1.log_predictive_trace, color = :red, linewidth=2, label = "VI (m=1)")
+lines!(ax, res10.times[2:end], res10.log_predictive_trace, color = :blue, linewidth=2, label = "VI (m=10)")
+axislegend(position=:lb)
 fig
+save("../figures/log_predictive_vs_time.pdf",fig)
