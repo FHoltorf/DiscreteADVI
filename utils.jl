6b3b1deb8384fe80bf85dd8f9d1e2e77a849c36d
Flemming Holtorf <holtorf@mit.edu>
2023-05-12 01:48:25 -0400

    improved plotting and other utilities

--- utils.jl
+++ utils.jl
@@ -1,52 +1,129 @@
-function elbo_estimator(ϕ; n=1, y=Y, prior=prior, W=W, X=X)
+struct ELBO{oType}
+    om::oType
+    n_estim::Int
+end
+function (elbo::ELBO)(ϕ)
+    return elbo_estimator(ϕ; n=elbo.n_estim, y=elbo.om.Y, z_known=elbo.om.z_known, prior=elbo.om.prior, W=elbo.om.W, X=elbo.om.X)
+end
+function elbo_estimator(ϕ; z_known=z_known, n=1, y=Y, prior=prior, W=W, X=X)
     elbo = 0.0
     for _ in 1:n
         z, αβ = sample_q(ϕ)
-        elbo += div(ϕ, z, αβ, y=y, prior=prior, W=W, X=X)
+        elbo += div(ϕ, z, αβ, y, z_known, prior, W, X)
     end
     return elbo/n
 end
 
-function predictive_posterior_estimator(ϕ; n=1, y=Y, W=W, X=X)
-    log_predictive = 0.0
+struct ELBO_marginal{oType}
+    om::oType
+    n_estim::Int
+end
+function (elbo::ELBO_marginal)(ϕ)
+    return elbo_estimator_marginal(ϕ; n=elbo.n_estim, y=elbo.om.Y, z_known=elbo.om.z_known, prior=elbo.om.prior, W=elbo.om.W, X=elbo.om.X)
+end
+function elbo_estimator_marginal(ϕ; n=1, y=Y, z_known=z_known, prior=prior, W=W, X=X)
+    elbo = 0.0
     for _ in 1:n
-        z, αβ = sample_q(ϕ)
-        log_predictive += loglikelihood(y, z, αβ, W=W, X=X)
+        αβ = sample_q_marginal(ϕ)
+        elbo += div_marginal(ϕ, αβ, y, z_known, prior, W, X)
+    end
+    return elbo/n
+end
+
+function predictive_posterior_estimator(ϕ, y, w, x, z_known; n=1, marginal = true)
+    log_predictive = 0.0
+    if marginal 
+        for _ in 1:n
+            z, αβ = sample_q(ϕ)
+            log_predictive += logmarginal(y, z_known, αβ, w, x) 
+        end
+    else
+        for _ in 1:n
+            z, αβ = sample_q(ϕ)
+            log_predictive += logconditional(y, z, αβ, w, x, z_known=z_known)
+        end
     end
     return log_predictive/n
 end
+predictive_posterior_estimator(ϕ, om::OccupancyModel; n=1, marginal = true) = predictive_posterior_estimator(ϕ, om.Y, om.W, om.X, om.z_known, n=n, marginal = marginal)
 
-function optimize_elbo(ϕ0, batch_size, iterations, LEARNING_RATE; n_estimator = batch_size, n_snapshots = 1)
-    obj = StochasticModel(ϕ -> -elbo_estimator(ϕ, n= batch_size), ϕ0)
-    elbo_trace = Float64[]
+function estimate_gradient(obj, n_estim)
+    grad = stochastic_gradient(obj)
+    grad_val = grad.p
+
+    for _ in 1:n_estim-1
+        grad_val += stochastic_gradient(obj).p
+    end
+    StochasticModel(grad.X, grad_val/n_estim)
+end
+function optimize_elbo(om::OccupancyModel, ϕ0, batch_size, iterations, LEARNING_RATE; n_switch = 100, n_estimator = batch_size, n_snapshots = 1, marginal = true)
+    elbo_trace = Float64[] 
     log_predictive_trace = Float64[]
     wall_times = Float64[]
     ϕ_trace = Vector{Float64}[]
-    optimizer = Adam(LEARNING_RATE)
+    
+    elbo_opt = ELBO(om, 1) #elbo_estimator(obj.p, n=n_estimator)
+    elbo_est = ELBO(om, n_estimator)
+    obj = StochasticModel(ϕ -> -elbo_opt(ϕ), ϕ0)
+    optimizer = Adam(LEARNING_RATE) #Optimiser(ExpDecay Adam())
     setup = Optimisers.setup(optimizer, obj)
+
+    elbo = elbo_est(obj.p)
     t = 0
-    elbo = elbo_estimator(obj.p, n=n_estimator)
     push!(elbo_trace, elbo)
     push!(wall_times, t)
+    k = 1
+    switched = false
     for i in 1:iterations
-        t += @elapsed grad = stochastic_gradient(obj)
+        t += @elapsed grad = estimate_gradient(obj, batch_size) #stochastic_gradient(obj)
         if sum(isinf.(grad.p)) == 0
+            if i > n_switch #&& !switched 
+                #optimizer = Adam(LEARNING_RATE/(i-n_switch)^(1/2 + 1e-8))
+                #setup = Optimisers.setup(optimizer, obj)
+                Optimisers.adjust!(setup, eta = LEARNING_RATE/(i-n_switch)^(1/2 + 1e-8))
+            end
             t += @elapsed Optimisers.update!(setup, obj, grad)
             push!(ϕ_trace, deepcopy(obj.p))
             println("iteration $i")
         end
         if i % n_snapshots == 0
-            elbo = elbo_estimator(obj.p, n=n_estimator)
-            log_predictive = predictive_posterior_estimator(obj.p, n=n_estimator)
+            elbo = elbo_est(obj.p) #elbo_estimator(obj.p, n=n_estimator)
+            log_predictive = predictive_posterior_estimator(obj.p, om, n=n_estimator, marginal = marginal)
             push!(log_predictive_trace, log_predictive)
             push!(elbo_trace, elbo)
             push!(wall_times, t)
         end
     end
-    elbo = elbo_estimator(obj.p, n=n_estimator)
-    log_predictive = predictive_posterior_estimator(obj.p, n=n_estimator)
+    elbo = elbo_est(obj.p)
+    log_predictive = predictive_posterior_estimator(obj.p, om, n=n_estimator, marginal = marginal)
     push!(log_predictive_trace, log_predictive)
     push!(elbo_trace, elbo)
     push!(wall_times, t)
     return obj.p, ϕ_trace, wall_times, elbo_trace, log_predictive_trace
+end
+
+function compute_mean_trace(chain, parnames, true_mean) 
+    data = chain[parnames].value.data[:,:,1]
+    agg_data = cumsum(data, dims=1)
+    means = agg_data ./ (1:size(data,1))
+    means .- true_mean'
+end
+
+# hardcoded for JAGS output
+function compute_log_posterior_trace(chain, names, om, mode = :marginal)
+    if mode == :marginal 
+        log_predictive_trace =  cumsum([logmarginal(om.Y,om.z_known,ab,om.W,om.X) for ab in eachrow(chain[names].value.data[:,:,1])]) 
+    else
+        log_predictive_trace = []
+        last_entry = 0
+        ab_range = chain[names[1:4]].value.data[:,:,1]
+        z_range = chain[names[5:end]].value.data[:,:,1]
+        for i in 1:size(chain,1)
+            ab = ab_range[i,:]
+            z = z_range[i,:]
+            push!(log_predictive_trace, last_entry + logconditional(om.Y,z,ab, om.W, om.X))         
+            last_entry = log_predictive_trace[end]
+        end
+    end
+    log_predictive_trace ./ (1:length(log_predictive_trace))
 end
--- visualizations.jl
+++ visualizations.jl
@@ -4,15 +4,15 @@ struct IntegerTicks end
 
 Makie.get_tickvalues(::IntegerTicks, vmin, vmax) = ceil(Int, vmin) : floor(Int, vmax)
 
-function plot_chain(chain; burnin = 1000)
+function plot_chain(chain; parnames = [Symbol("αβ[1]"), Symbol("αβ[2]"), Symbol("αβ[3]"), Symbol("αβ[4]")], burnin = 1000)
     fig = Figure(fontsize=28)
     ax_α = Axis(fig[1,1], xlabel = L"\alpha_1", ylabel=L"\alpha_2")
-    scatter!(ax_α, chain.value[burnin:end, chain.value.axes[2][1]].data[:], 
-                   chain.value[burnin:end, chain.value.axes[2][2]].data[:], 
+    scatter!(ax_α, chain[parnames[1]].data[burnin:end], 
+                   chain[parnames[2]].data[burnin:end], 
                    color = (:black,0.5))
     ax_β = Axis(fig[1,2], xlabel = L"\beta_1", ylabel=L"\beta_2")
-    scatter!(ax_β, chain.value[burnin:end, chain.value.axes[2][3]].data[:], 
-                   chain.value[burnin:end, chain.value.axes[2][4]].data[:], 
+    scatter!(ax_β, chain[parnames[3]].data[burnin:end], 
+                   chain[parnames[4]].data[burnin:end], 
                    color = (:black,0.5))
     fig
 end
@@ -30,12 +30,11 @@ function plot_gaussian(Σ, μ, width)
     return X,Y,Z
 end
 
-function pairplot(chain, ϕ, true_pars; burnin = 5000, width = 5, thinning = 1000)
-    μ, V = ϕ[1:4], reshape(ϕ[5:20],4,4)
-    Σ = V*V' + 0.01*I
-    symbols = [Symbol("αβ[$i]") for i in 1:4]
+function pairplot(chain, ϕ, true_pars; parnames = [Symbol("αβ[1]"), Symbol("αβ[2]"), Symbol("αβ[3]"), Symbol("αβ[4]")], 
+                                       burnin = 5000, width = 5, thinning = 1000)
+    μ, Σ = reshape_params(ϕ)
     vars = [L"\alpha_1", L"\alpha_2", L"\beta_1", L"\beta_2"]
-    chain_mean = [mean(chain[par][burnin:end]) for par in symbols]
+    chain_mean = [mean(chain[par][burnin:end]) for par in parnames]
 
     fig = Figure(fontsize=28, resolution = (1000,1000))
 
@@ -45,8 +44,8 @@ function pairplot(chain, ϕ, true_pars; burnin = 5000, width = 5, thinning = 100
         for j in i+1:4
             k += 1
             X, Y, Z = plot_gaussian(Σ[[j,i],[j,i]], μ[[j,i]], width)
-            scatter!(axs[i][k], chain[symbols[j]][burnin:end].data[1:thinning:end], 
-                                chain[symbols[i]][burnin:end].data[1:thinning:end], 
+            scatter!(axs[i][k], chain[parnames[j]][burnin:end].data[1:thinning:end], 
+                                chain[parnames[i]][burnin:end].data[1:thinning:end], 
                                 color = (:black,0.2))
             global cf = contour!(axs[i][k], X, Y, -Z, linewidth = 2, 
                                 colorrange = (-width, 0), 
@@ -86,15 +85,14 @@ function pairplot(chain, ϕ, true_pars; burnin = 5000, width = 5, thinning = 100
 end
 
 # plot a movie 
-function pairplot_movie(chain, ϕ_trace, true_pars; filename = "VI_movie.mp4", burnin = 5000, width = 5, thinning = 1000)
+function pairplot_movie(chain, ϕ_trace, true_pars; parnames = [Symbol("αβ[1]"), Symbol("αβ[2]"), Symbol("αβ[3]"), Symbol("αβ[4]")],
+                                                   filename = "VI_movie.mp4", burnin = 5000, width = 5, thinning = 1000)
     ϕ = ϕ_trace[end]
     obs = Dict()
 
-    μ, V = ϕ[1:4], reshape(ϕ[5:20],4,4)
-    Σ = V*V' + 0.01*I
-    symbols = [Symbol("αβ[$i]") for i in 1:4]
+    μ, Σ = reshape_params(ϕ)
     vars = [L"\alpha_1", L"\alpha_2", L"\beta_1", L"\beta_2"]
-    chain_mean = [mean(chain[par][burnin:end]) for par in symbols]
+    chain_mean = [mean(chain[par][burnin:end]) for par in parnames]
 
     fig = Figure(fontsize=28, resolution = (1000,1000))
 
@@ -104,8 +102,8 @@ function pairplot_movie(chain, ϕ_trace, true_pars; filename = "VI_movie.mp4", b
         k = 0
         for j in i+1:4
             k += 1
-            scatter!(axs[i][k], chain[symbols[j]][burnin:end].data[1:thinning:end], 
-                                chain[symbols[i]][burnin:end].data[1:thinning:end], 
+            scatter!(axs[i][k], chain[parnames[j]][burnin:end].data[1:thinning:end], 
+                                chain[parnames[i]][burnin:end].data[1:thinning:end], 
                                 color = (:black,0.2))
             scatter!(axs[i][k], [chain_mean[j]], [chain_mean[i]], 
                                 color = :white, strokecolor=:black, strokewidth=1, marker = :cross, markersize=15)
@@ -156,37 +154,44 @@ function pairplot_movie(chain, ϕ_trace, true_pars; filename = "VI_movie.mp4", b
     end
 end
 
-function marginals(chain, ϕ; burnin = 1000, width = 4, opacity=0.1)
-    μ, V = ϕ[1:4], reshape(ϕ[5:20],4,4)
-    Σ = V*V' + 0.01*I
+function marginals(chain, ϕ;parnames = [Symbol("αβ[1]"), Symbol("αβ[2]"), Symbol("αβ[3]"), Symbol("αβ[4]")], 
+                            burnin = 1000, linecolor=:red, width = 4, opacity=0.1, n_bins = 20)
+    μ, Σ = reshape_params(ϕ)
     vars = [L"\alpha_1", L"\alpha_2", L"\beta_1", L"\beta_2"]
-    fig = Figure(fontsize=24, resolution = (1100,500))
+    fig = Figure(fontsize=24, resolution = (1500,500))
     axs = [Axis(fig[1,i], xminorgridvisible=false, xgridvisible=false,
                           yminorgridvisible=false, ygridvisible=false,
-                          xlabel = vars[i]) for i in 1:4]
+                          xlabel = vars[i],
+                          ylabel = i == 1 ? "density" : "") for i in 1:4]
     for i in 1:4
         dist = Normal(μ[i], sqrt.(Σ[i,i]))
         x_range = range(dist.μ-width*dist.σ, dist.μ+width*dist.σ, 100)
-        samples = chain.value[burnin:end, chain.value.axes[2][i]].data[:]
-        hist!(axs[i], samples, normalization=:pdf, strokcolor=:black, strokewidth=2, color=(:gray, opacity))
-        lines!(axs[i], x_range, pdf.(dist, x_range), color = :red, linewidth=2)
+        samples = chain[parnames[i]].data[burnin:end]
+        hist!(axs[i], samples, bins = n_bins, normalization=:pdf, strokcolor=:black, strokewidth=2, 
+                               color=(:gray, opacity), label = i == 4 ? "HMC" : missing)
+        lines!(axs[i], x_range, pdf.(dist, x_range), 
+                                color = linecolor, linewidth=2, label = i == 4 ? "VI" : missing)
     end
-    
+    axislegend()
     fig
 end
 
-function PAO_plot(ϕ_opt, chain; z_known=z_known)
+function PAO_plot(ϕ_opt, chain, om)
+    W = om.W
+    X = om.X
+    n = om.n 
+    z_known = om.z_known
+
     unknown = filter(i -> !z_known[i], 1:n)
-    μ = ϕ_opt[1:4]
-    V = reshape(ϕ_opt[5:20], 4, 4)
-    αβ_post = MvNormal(μ, Hermitian(V*V'+0.01*I))
+    μ, Σ = reshape_params(ϕ_opt)
+    αβ_post = MvNormal(μ, Hermitian(Σ))
     n_samples = 100000
     NO = zeros(Int,n-sum(z_known)+1)
     for _ in 1:n_samples
         αβ = rand(αβ_post)
         @views α, β = αβ[1:2], αβ[3:4]
-        pys = [prod(d(W[i][j], α) for j in eachindex(W[i])) for i in unknown]
-        ps = [ψ(X[i],β)*pys[k]/(1+pys[k]) for (k,i) in enumerate(unknown)]
+        pys = [prod(1-d(W[i][j], α) for j in eachindex(W[i])) for i in unknown]
+        ps = [ψ(X[i],β)*pys[k]/(1-ψ(X[i],β)+ψ(X[i],β)*pys[k]) for (k,i) in enumerate(unknown)]
         val = sum(rand(Bernoulli(p)) for p in ps)
         NO[val+1] +=1
     end
@@ -217,7 +222,127 @@ function PAO_plot(ϕ_opt, chain; z_known=z_known)
     fig
 end
 
+function PAO_plot_JAGS(ϕ_opt, chain, om)
+    n = om.n
+    z_known = om.z_known
+    W = om.W
+    X = om.X
+    
+    unknown = filter(i -> !z_known[i], 1:n)
+    μ, Σ = reshape_params(ϕ_opt)
+    αβ_post = MvNormal(μ, Hermitian(Σ))
+    n_samples = 100000
+    NO = zeros(Int,n-sum(z_known)+1)
+    for _ in 1:n_samples
+        αβ = rand(αβ_post)
+        @views α, β = αβ[1:2], αβ[3:4]
+        pys = [prod(1-d(W[i][j], α) for j in eachindex(W[i])) for i in unknown]
+        ps = [ψ(X[i],β)*pys[k]/(1 - ψ(X[i],β) + ψ(X[i],β)*pys[k]) for (k,i) in enumerate(unknown)]
+        val = sum(rand(Bernoulli(p)) for p in ps)
+        NO[val+1] +=1
+    end
 
+    NO_chain = zeros(Int, n - sum(z_known) + 1)
+    for z in eachrow(chain.value[25000:end,3:end-2,1])
+        val = round(Int, sum(z)) - sum(z_known)
+        NO_chain[val+1] += 1
+    end
+    NO_chain_dist = (NO_chain)./sum(NO_chain)
+    NO_dist = (NO)./sum(NO)
+    fig = Figure(fontsize=28)
+    ax = Axis(fig[1,1], xlabel = "number of occupied sites", 
+                        ylabel = "posterior predictive probability mass",
+                        xminorgridvisible=false,
+                        xgridvisible=false,
+                        yminorgridvisible=false,
+                        ygridvisible=false)
+    barplot!(ax, collect(sum(z_known):n) .- 1/4, NO_chain_dist, width=1/2, color = :black, label = "HMC")
+    barplot!(ax, collect(sum(z_known):n) .+ 1/4, NO_dist, width=1/2, 
+                color = :lightgray, strokecolor = :black, strokewidth=1, label = "VI")
+    text!(ax, Point2(0.4, 0.875), space = :relative, text="TV-distance = $(round(50*norm(NO_chain_dist-NO_dist, 1), digits=1))%",
+                                fontsize=28)
+    axislegend(position = :rt)
+    fig
+end
+
+
+function log_predictive_trace_plot(times, traces, names; ymin = -300, colors = [:black, :red])
+    fig = Figure(fontsize=28)
+    ax = Axis(fig[1,1], xlabel = "time [s]", 
+                        xminorgridvisible = true,
+                        xminorticks = IntervalsBetween(9),
+                        ylabel = "log predictive posterior",
+                        xticks = LogTicks(IntegerTicks()),
+                        xscale = log10)
+    ymax = 5+maximum(maximum.(traces))
+    ylims!(ax, ymin, ymax)
+    for k in eachindex(times)
+        lines!(ax, times[k], traces[k], label = names[k], color = colors[k], linewidth = 2)
+    end
+    axislegend(position=:lb)
+    fig
+end
+
+function trace_plot(times, traces, names; xscale=log10, yscale=log10, ylabel = "", xlabel ="time [s]", ymin = nothing, ymax=nothing, colors = [:black, :red])
+    fig = Figure(fontsize=28)
+    ax = Axis(fig[1,1], xlabel = xlabel, 
+                        xminorgridvisible = true,
+                        xminorticks = IntervalsBetween(9),
+                        ylabel = ylabel,
+                        yminorgridvisible = true,
+                        yminorticks = IntervalsBetween(9),
+                        xticks = LogTicks(IntegerTicks()),
+                        xscale = xscale,
+                        yscale = yscale) 
+   # ylims!(ax, ymin, ymax)
+    for k in eachindex(times)
+        lines!(ax, times[k], traces[k], label = names[k], color = colors[k], linewidth = 2)
+    end
+    axislegend(position=:lb)
+    fig
+end
+
+function trace_plot_with_std(times, mean_traces, std_traces, names; xscale=log10, yscale=log10, ylabel = "", xlabel ="time [s]", ymin = nothing, ymax=nothing, colors = [:black, :red])
+    fig = Figure(fontsize=28)
+    ax = Axis(fig[1,1], xlabel = xlabel, 
+                        xminorgridvisible = true,
+                        xminorticks = IntervalsBetween(9),
+                        ylabel = ylabel,
+                        yminorgridvisible = true,
+                        yminorticks = IntervalsBetween(9),
+                        xticks = LogTicks(IntegerTicks()),
+                        xscale = xscale,
+                        yscale = yscale) 
+   # ylims!(ax, ymin, ymax)
+    for k in eachindex(times)
+        lines!(ax, times[k], mean_traces[k], label = names[k], color = colors[k], linewidth = 2)
+        band!(ax, times[k], mean_traces[k] - std_traces[k], mean_traces[k] + std_traces[k], color = (colors[k], 0.2))
+    end
+    axislegend(position=:lb)
+    fig
+end
+
+function trace_plot_with_spreads(times, mean_traces, quants, names; xscale=log10, yscale=log10, ylabel = "", xlabel ="time [s]", ymin = nothing, ymax=nothing, colors = [:black, :red])
+    fig = Figure(fontsize=28)
+    ax = Axis(fig[1,1], xlabel = xlabel, 
+                        xminorgridvisible = true,
+                        xminorticks = IntervalsBetween(9),
+                        yminorgridvisible = yscale == log10 ? true : false,
+                        yminorticks = IntervalsBetween(9),
+                        ylabel = ylabel,
+                        xticks = LogTicks(IntegerTicks()),
+                        xscale = xscale,
+                        yscale = yscale) 
+    if !isnothing(ymax) || !isnothing(ymax)
+        ylims!(ax, (ymin, ymax))
+    end
+    for k in eachindex(times)
+        lines!(ax, times[k], mean_traces[k], label = names[k], color = colors[k], linewidth = 2)
+        band!(ax, times[k], quants[k][1], quants[k][2], color = (colors[k], 0.2))
+    end
+    axislegend(position=:lb)
+    fig
+end
 # plot convergence
 #=
 batch_sizes = [1, 10, 50]
